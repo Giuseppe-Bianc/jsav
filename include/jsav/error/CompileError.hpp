835a1954c2b69e06963e05c02ce524193c826113
GIUSEPPE BIANCONI <gbianconi3@gmail.com>
2026-03-20 22:57:29 +0100

    refactor(error): enable SyntaxError handling in CompileError
    - Updated `CompileError.hpp` to uncomment `SyntaxError` enum and method
    - Modified `CompileError.cpp` to implement `SyntaxError` handling
    - Adjusted `ErrorReporter.cpp` to handle `SyntaxError` in error reporting
    - Revised `error_codes.hpp` to include `Parser` phase in `CompilerPhase` enum
    - Cleaned up commented-out code for clarity and maintainability
    
    These changes enhance the error handling capabilities of the compiler by
    allowing for `SyntaxError` reporting, which is essential for future
    syntax analysis features. The refactoring improves code readability
    and prepares the codebase for further development of error types.

--- include/jsav/error/CompileError.hpp
+++ include/jsav/error/CompileError.hpp
@@ -15,8 +15,8 @@ namespace jsv {
     public:
         enum class Kind {
             LexerError,
-            /*SyntaxError,
-            TypeError,
+            SyntaxError,
+            /*TypeError,
             IrGeneratorError,
             AsmGeneratorError,
             IoError,*/
@@ -27,14 +27,10 @@ namespace jsv {
         static CompileError LexerError(std::optional<ErrorCode> code, std::string_view message, const SourceSpan &span,
                                        std::optional<std::string> help);
 
-        /*static CompileError SyntaxError(
-        std::optional<ErrorCode> code,
-        std::string_view message,
-        SourceSpan span,
-        std::optional<std::string> help
-    );
+        static CompileError SyntaxError(std::optional<ErrorCode> code, std::string_view message, const SourceSpan &span,
+                                        std::optional<std::string> help);
 
-    static CompileError TypeError(
+    /*static CompileError TypeError(
         std::optional<ErrorCode> code,
         std::string_view message,
         SourceSpan span,
